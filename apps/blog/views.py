711fb16f4a3542e31cd657c779021aa5f8c875dd
William Tran <wtran29@gmail.com>
2018-07-13 12:52:40 -0700

    added final features

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
 from .models import Blog
 # Create your views here.
 
@@ -12,6 +12,6 @@ def allblogs(request):
 
 def detail(request, blog_id):
     context = {
-        'blog': Blog.objects.get(pk=blog_id)
+        'blog': get_object_or_404(Blog, pk=blog_id)
     }
     return render(request, 'blog/blog.html', context)
