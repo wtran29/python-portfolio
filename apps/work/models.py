e5253c370911cb5db8136ea836f59940221689b9
William Tran <wtran29@gmail.com>
2018-09-20 12:36:56 -0700

    updated resume

--- apps/work/models.py
+++ apps/work/models.py
@@ -1,15 +1,14 @@
 from django.db import models
-from django.contrib.postgres.fields import ArrayField
+
 # Create your models here.
 
 
 class Work(models.Model):
     title = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/', blank=True)
     summary = models.TextField()
     video = models.CharField(max_length=5000, blank=True)
     description = models.TextField()
-    tech = ArrayField(models.CharField(max_length=1000))
     github = models.CharField(max_length=5000, blank=True)
     deploy = models.CharField(max_length=5000, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
