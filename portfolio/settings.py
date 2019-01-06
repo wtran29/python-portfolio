fb4a563dea4e14cbf3ec618c89656da2971dbd48
William Tran <wtran29@gmail.com>
2019-01-05 17:30:13 -0800

    minor changes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'python-portfolio/static')
+    os.path.join(BASE_DIR, 'portfolio/static')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
