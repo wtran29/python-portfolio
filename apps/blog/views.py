991701e7699b8ba85dc3442f77ab64770ace52dc
William Tran <wtran29@gmail.com>
2019-01-16 17:34:57 -0800

    implement replies to comments, use jquery for dynamic effect

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -61,12 +61,28 @@ def detail(request, blog_id):
         content_type = ContentType.objects.get(model=c_type)
         obj_id = comment_form.cleaned_data.get("object_id")
         content_data = comment_form.cleaned_data.get("content")
+        # Make sure parent obj is there
+        parent_obj = None
+        # check/parse to see if parent id is there
+        try:
+            parent_id = int(request.POST.get("parent_id"))
+        except:
+            parent_id = None
+        # check if the parent id is in the database,
+        # if it exists, we set the parent obj to be the first item in qs
+        if parent_id:
+            parent_qs = Comment.objects.filter(id=parent_id)
+            if parent_qs.exists and parent_qs.count() == 1:
+                parent_obj = parent_qs.first()
+
         new_comment, created = Comment.objects.get_or_create(
             user=request.user,
             content_type=content_type,
             object_id=obj_id,
-            content=content_data
+            content=content_data,
+            parent=parent_obj,
         )
+        return redirect(new_comment.content_object.get_absolute_url())
 
     # This gives all the comments using BlogManager
     comments = blog.comments  # Getting the comments as a property vs qs - Comment.objects.filter_by_instance(blog)
--- apps/comments/models.py
+++ apps/comments/models.py
@@ -6,12 +6,16 @@ from django.conf import settings
 
 
 class CommentManager(models.Manager):
+    def all(self):
+        qs = super(CommentManager, self).filter(parent=None)
+        return qs
+
     def filter_by_instance(self, instance):
         # Getting the content type from Blog model
         content_type = ContentType.objects.get_for_model(instance.__class__)
         obj_id = instance.id  # Same as Blog.objects.get(id=blog.id)
         # This gives all the comments related to the blog
-        qs = super(CommentManager, self).filter(content_type=content_type, object_id=obj_id)
+        qs = super(CommentManager, self).filter(content_type=content_type, object_id=obj_id).filter(parent=None)
         return qs
 
 
@@ -21,13 +25,24 @@ class Comment(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True)
     object_id = models.PositiveIntegerField(null=True)
     content_object = GenericForeignKey('content_type', 'object_id')
-
+    parent = models.ForeignKey("self", null=True, blank=True, on_delete=models.CASCADE)
     content = models.TextField()
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     objects = CommentManager()
 
+    class Meta:
+        ordering = ['-created_at']
+
     def __str__(self):
         return str(self.user.username)
 
+    def children(self):  # Replies to comments
+        return Comment.objects.filter(parent=self).order_by("created_at")
+
+    @property
+    def is_parent(self):
+        if self.parent is not None:
+            return False
+        return True
