498027b0466e052de97ac1db1f8ea48bf367c676
William Tran <wtran29@gmail.com>
2019-01-17 23:47:05 -0800

    implement SerializerMethodField from serializers to get image url, html content, and username to display on rest api

--- apps/blog/api/serializers.py
+++ apps/blog/api/serializers.py
@@ -1,4 +1,8 @@
-from rest_framework.serializers import ModelSerializer, HyperlinkedIdentityField
+from rest_framework.serializers import (
+    ModelSerializer,
+    HyperlinkedIdentityField,
+    SerializerMethodField,
+)
 
 from apps.blog.models import Blog
 
@@ -12,6 +16,27 @@ blog_detail_url = HyperlinkedIdentityField(
 
 class BlogListSerializer(ModelSerializer):
     url = blog_detail_url
+    user = SerializerMethodField()
+
+    class Meta:
+        model = Blog
+        fields = [
+            "url",
+            "user",
+            "title",
+            "body",
+            "pub_date",
+        ]
+
+    def get_user(self, obj):
+        return str(obj.user.username)
+
+
+class BlogDetailSerializer(ModelSerializer):
+    url = blog_detail_url
+    user = SerializerMethodField()
+    image = SerializerMethodField()
+    html = SerializerMethodField()
 
     class Meta:
         model = Blog
@@ -21,22 +46,23 @@ class BlogListSerializer(ModelSerializer):
             "user",
             "title",
             "body",
+            "html",
             "pub_date",
+            "image"
         ]
 
+    def get_html(self, obj):
+        return obj.get_html()
 
-class BlogDetailSerializer(ModelSerializer):
-    url = blog_detail_url
+    def get_user(self, obj):
+        return str(obj.user.username)
 
-    class Meta:
-        model = Blog
-        fields = [
-            "url",
-            "id",
-            "title",
-            "body",
-            "pub_date",
-        ]
+    def get_image(self, obj):
+        try:
+            image = obj.image.url
+        except:
+            image = None
+        return image
 
 
 class BlogCreateUpdateSerializer(ModelSerializer):
