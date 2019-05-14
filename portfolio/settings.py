449f0843e65e7f423d2b969d9560e0121b55b21d
William Tran <wtran29@gmail.com>
2019-05-14 08:34:01 -0700

    allow browsable api render for rest framework

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -148,8 +148,8 @@ MEDIA_URL = '/media/'
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-        # 'rest_framework.renderers.BrowsableAPIRenderer',
+        # 'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     # 'DEFAULT_PARSER_CLASSES': (
     #     'rest_framework.parsers.JSONParser',
