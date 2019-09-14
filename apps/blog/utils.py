4c7af0e13863418c201136c265c44f5d580775ff
William Tran <wtran29@gmail.com>
2019-09-13 19:15:05 -0700

    updated blog site

--- apps/blog/utils.py
+++ apps/blog/utils.py
@@ -21,8 +21,8 @@ def get_read_time(html_string):
 
 
 def get_header_text():
-    header_text = ['If you heard it from us, then it\'s possibly true.', 'The #1 source of real fake news.',
-                   'Sometimes it\'s not what you said, it\'s how you said it.',
-                   'There is fake news. And there is REAL fake news.',
+    header_text = ['Believe in Billy', 'Trust my sources. I researched this shit.',
+                   'I AM THE BEST. Daily affirmation...',
+                   'Sometimes I\'ll start a sentence and I don\'t even know where it\'s going. I just hope I find it along the way.',
                    'Numbers don\'t lie, but then there are imaginary numbers.']
     return header_text
